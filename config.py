42a60722e6adf01071a5d50f3760930f272f628a
ChrisSR247 <nplok2690@gmail.com>
2026-07-31 13:10:45 -0500

    Correciones

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(BASE_DIR / ".env")
 
-
+#holaaaaaaaaaaaaaaa
 # ============================================================
 # API NVIDIA NIM
 # ============================================================
